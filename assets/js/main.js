809f75d4b9d70ff557a46ffeb1ff86c30734e81e
Reina <20066312@tafe.wa.edu.au>
2026-06-24 01:39:57 +0800

    feat: Add tailwind styling to resume.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,30 +4,30 @@ function toggleSideNav(){
     const hideBtn = document.getElementById("collapse-btn")
     const asideNav = document.getElementById("aside_nav")
     const sidePanel = document.getElementById("side_panel")
-    if (sidePanel.className === "row-2 col-0 w-64 bg-[#271D4B] text-white transition-all duration-300 border shadow-sm border-slate-200 shadow-slate-950/5\n" +
-        "flex flex-col"){
+    if (sidePanel.className === "row-2 col-0 w-64 bg-[#271D4B] text-white transition-all duration-300 border\n"+
+        "shadow-sm border-slate-200 shadow-slate-950/5 flex flex-col"){
         asideNav.className = "hidden"
         asideTitle.className = "hidden"
-        hideBtn.className = "p-1 rounded-md !bg-[#E25600] hover:bg-gray-700 focus:outline-none"
+        hideBtn.className = "m-2! p-1 rounded-md! !bg-[#E25600] hover:bg-gray-700 focus:outline-none self-baseline!"
         sidePanel.className = "block w-[auto] h-[auto] bg-none"
     }
     else {
         asideNav.className = "mt-5 px-2"
-        asideTitle.className = "font-sans antialiased text-base font-semibold text-white"
-        hideBtn.className = "p-1 h-8 w-8 rounded-md !bg-[#E25600] hover:bg-gray-700 focus:outline-none"
-        sidePanel.className = "row-2 col-0 w-64 bg-[#271D4B] text-white transition-all duration-300 border shadow-sm border-slate-200 shadow-slate-950/5\n" +
-            "flex flex-col"
+        asideTitle.className = "font-inconsolata antialiased text-left! text-[24px] font-semibold text-white underline! py-4! pl-3!"
+        hideBtn.className = "m-2! p-1 h-8 w-8 rounded-md! !bg-[#E25600] hover:bg-gray-700 focus:outline-none self-baseline!"
+        sidePanel.className = "row-2 col-0 w-64 bg-[#271D4B] text-white transition-all duration-300 border\n" +
+            "shadow-sm border-slate-200 shadow-slate-950/5 flex flex-col"
     }
 }
 
 function expandSideNav(){
     const projectBtn = document.getElementById("projectsBtn_side")
     const projectsList = document.getElementById("sidebarCollapseList")
-    if (projectsList.className !== "dropdown-content flex flex-col gap-0.5 p-[2px] space-y-1 bg-[#271D4B]"){
-        projectsList.className = "dropdown-content flex flex-col gap-0.5 p-[2px] space-y-1 bg-[#271D4B]"
+    if (projectsList.className !== "dropdown-content flex flex-col w-[90%]! pt-0.75! px-2! gap-0.5! justify-self-end bg-[#271D4B]"){
+        projectsList.className = "dropdown-content flex flex-col w-[90%]! pt-0.75! px-2! gap-0.5! justify-self-end bg-[#271D4B]"
     }
     else {
-        projectsList.className = "dropdown-content hidden p-[2px] space-y-1 bg-[#271D4B]"
+        projectsList.className = "dropdown-content hidden w-[90%]! pt-0.75! px-2! gap-0.5! justify-self-end bg-[#271D4B]"
     }
 }
 
