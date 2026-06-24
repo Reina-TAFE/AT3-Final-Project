d1cfb32889e66cb98c0f7a8d82a28fb309b84a44
Reina <20066312@tafe.wa.edu.au>
2026-06-24 20:16:55 +0800

    feat: validate main.js with jshint

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,7 @@ function removeError()
 }
 
 function validateForm(e) {
-    'use strict'
+    'use strict';
     // Get the event object:
 
     // Get form references:
@@ -84,36 +84,40 @@ function validateForm(e) {
         addError(errorMessage);
         return false;
     }
+    else
+    {
+        removeError();
+    }
 }
 
 function toggleSideNav(){
-    const asideTitle = document.getElementById("aside_title")
-    const hideBtn = document.getElementById("collapse-btn")
-    const asideNav = document.getElementById("aside_nav")
-    const sidePanel = document.getElementById("side_panel")
+    const asideTitle = document.getElementById("aside_title");
+    const hideBtn = document.getElementById("collapse-btn");
+    const asideNav = document.getElementById("aside_nav");
+    const sidePanel = document.getElementById("side_panel");
     if (sidePanel.className === "row-2 col-0 w-64 bg-[#271D4B] text-white transition-all duration-300 border\n"+
         "shadow-sm border-slate-200 shadow-slate-950/5 flex flex-col"){
-        asideNav.className = "hidden"
-        asideTitle.className = "hidden"
-        hideBtn.className = "m-2! p-1 rounded-md! !bg-[#E25600] hover:bg-gray-700 focus:outline-none self-baseline!"
-        sidePanel.className = "block w-[auto] h-[auto] bg-none"
+        asideNav.className = "hidden";
+        asideTitle.className = "hidden";
+        hideBtn.className = "m-2! p-1 rounded-md! !bg-[#E25600] hover:bg-gray-700 focus:outline-none self-baseline!";
+        sidePanel.className = "block w-[auto] h-[auto] bg-none";
     }
     else {
-        asideNav.className = "mt-5 px-2"
-        asideTitle.className = "font-inconsolata antialiased text-left! text-[24px] font-semibold text-white underline! py-4! pl-3!"
-        hideBtn.className = "m-2! p-1 h-8 w-8 rounded-md! !bg-[#E25600] hover:bg-gray-700 focus:outline-none self-baseline!"
+        asideNav.className = "mt-5 px-2";
+        asideTitle.className = "font-inconsolata antialiased text-left! text-[24px] font-semibold text-white underline! py-4! pl-3!";
+        hideBtn.className = "m-2! p-1 h-8 w-8 rounded-md! !bg-[#E25600] hover:bg-gray-700 focus:outline-none self-baseline!";
         sidePanel.className = "row-2 col-0 w-64 bg-[#271D4B] text-white transition-all duration-300 border\n" +
-            "shadow-sm border-slate-200 shadow-slate-950/5 flex flex-col"
+            "shadow-sm border-slate-200 shadow-slate-950/5 flex flex-col";
     }
 }
 
 function expandSideNav(){
-    const projectsList = document.getElementById("sidebarCollapseList")
+    const projectsList = document.getElementById("sidebarCollapseList");
     if (projectsList.className !== "dropdown-content flex flex-col w-[90%]! pt-0.75! px-2! gap-0.5! justify-self-end bg-[#271D4B]"){
-        projectsList.className = "dropdown-content flex flex-col w-[90%]! pt-0.75! px-2! gap-0.5! justify-self-end bg-[#271D4B]"
+        projectsList.className = "dropdown-content flex flex-col w-[90%]! pt-0.75! px-2! gap-0.5! justify-self-end bg-[#271D4B]";
     }
     else {
-        projectsList.className = "dropdown-content hidden w-[90%]! pt-0.75! px-2! gap-0.5! justify-self-end bg-[#271D4B]"
+        projectsList.className = "dropdown-content hidden w-[90%]! pt-0.75! px-2! gap-0.5! justify-self-end bg-[#271D4B]";
     }
 }
 
